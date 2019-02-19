2419123d7259d2e8cbfb0e37f7dbe58b57c91f63
Andrew Lincoln <andrewjlincoln@gmail.com>
2019-02-18 22:01:09 -0800

    key fix

--- db/seeds/04-users_answers_questions.js
+++ db/seeds/04-users_answers_questions.js
@@ -11,20 +11,20 @@ exports.seed = function(knex, Promise) {
 
         {id: 4, user_id: 2, question_id: 1, answer:4},
         {id: 5, user_id: 2, question_id: 2, answer:3},
-        {id: 6, user_id: 2, question_id: 2, answer:2},
+        {id: 6, user_id: 2, question_id: 5, answer:2},
 
       
 
         {id: 7, user_id: 3, question_id: 1, answer:1},
         {id: 8, user_id: 3, question_id: 2, answer:4},
-        {id: 9, user_id: 3, question_id: 2, answer:2},
+        {id: 9, user_id: 3, question_id: 3, answer:2},
 
         {id: 10, user_id: 4, question_id: 1, answer:3},
         {id: 11, user_id: 4, question_id: 2, answer:4},
         {id: 12, user_id: 4, question_id: 3, answer:1},
 
         {id: 13, user_id: 5, question_id: 1, answer:2},
-        {id: 14, user_id: 5, question_id: 2, answer:4},
+        {id: 14, user_id: 5, question_id: 8, answer:4},
         {id: 15, user_id: 5, question_id: 3, answer:3},
 
         {id: 16, user_id: 6, question_id: 1, answer:3},
@@ -40,18 +40,18 @@ exports.seed = function(knex, Promise) {
         {id: 24, user_id: 8, question_id: 3, answer:2},
 
 
-        {id: 23, user_id: 9, question_id: 1, answer:2},
-        {id: 24, user_id: 9, question_id: 2, answer:4},
-        {id: 25, user_id: 9, question_id: 3, answer:1},
+        {id: 25, user_id: 9, question_id: 1, answer:2},
+        {id: 26, user_id: 9, question_id: 9, answer:4},
+        {id: 27, user_id: 9, question_id: 3, answer:1},
 
 
-        {id: 26, user_id: 10, question_id: 1, answer:2},
-        {id: 27, user_id: 10, question_id: 2, answer:2},
-        {id: 28, user_id: 10, question_id: 3, answer:4},
+        {id: 28, user_id: 10, question_id: 1, answer:2},
+        {id: 29, user_id: 10, question_id: 2, answer:2},
+        {id: 30, user_id: 10, question_id: 3, answer:4},
 
-        {id: 29, user_id: 11, question_id: 1, answer:1},
-        {id: 30, user_id: 11, question_id: 2, answer:3},
-        {id: 31, user_id: 11, question_id: 3, answer:4},
+        {id: 31, user_id: 11, question_id: 1, answer:1},
+        {id: 32, user_id: 11, question_id: 2, answer:3},
+        {id: 33, user_id: 11, question_id: 3, answer:4},
 
 
 
