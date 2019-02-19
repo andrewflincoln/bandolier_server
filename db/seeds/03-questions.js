655392e14c84b8525d7049860985f9c62260d52f
Andrew Lincoln <andrewjlincoln@gmail.com>
2019-02-18 21:56:29 -0800

    new questions seeds

--- db/seeds/03-questions.js
+++ db/seeds/03-questions.js
@@ -8,6 +8,14 @@ exports.seed = function(knex, Promise) {
         {id: 1, question_text: `How would you like a good review in Pitchfork?`, option_1: `It would be an honor!`, option_2: `Sure, why not?`, option_3: `No, I want them to hate me!`, option_4: `No, I'd just rather avoid it.`},
         {id: 2, question_text: `How good are you at your instrument?`, option_1: `I can barely play the thing.`, option_2: `Pretty good.`, option_3: `I shred.`, option_4: `I went to Julliard or some shit.`},
         {id: 3, question_text: `Would you participate in a huge reunion 20 years after your initially under-appreciated band's break up?`, option_1: `Yes! At last, they really love me!`, option_2: `Only if we're doing good new stuff.`, option_3: `Maybe if my solo thing isn't working out. I have a mortgage!`, option_4: `No fuckin way. Sorry you missed it, losers.`},
+        {id: 4, question_text: `Do you wish to rail against The Man?`, option_1: `Yes, endlessly.`, option_2: `Some.`, option_3: `The Man is bad, but not a musical priority.`, option_4: `The Man is good/I am The Man.`},
+        {id: 5, question_text: `What's your ideal tour lifestyle? `, option_1: `Wild.`, option_2: `Wild, in moderation.`, option_3: `Not too wild.`, option_4: `Serene.`},
+        {id: 6, question_text: `Who's the best?`, option_1: `Metallica`, option_2: `Megadeth`, option_3: `Slayer`, option_4: `None`},
+        {id: 7, question_text: `Who's the best?`, option_1: `Yeah Yeah Yeahs`, option_2: `The Strokes`, option_3: `Interpol`, option_4: `None`},
+        {id: 8, question_text: `Who's the best?`, option_1: `Nirvana`, option_2: `Pearl Jam`, option_3: `Soundgarden`, option_4: `None`},
+        {id: 9, question_text: `If your band has a plural name, do you want "The..." to be part of the name?`, option_1: `Yes`, option_2: `No`, option_3: `Depends`, option_4: `I don't think about such things.`},
+        {id: 10, question_text: `Are substances part of your creative process?`, option_1: `Essential.`, option_2: `Sometimes.`, option_3: `Not really, but they're around.`, option_4: `They're not around.`},
+
       ]).then(() => {
         return knex.raw(
           `SELECT setval('questions_id_seq', (SELECT MAX(id) FROM questions));`
@@ -15,5 +23,3 @@ exports.seed = function(knex, Promise) {
       })
     });
 };
-
-
--- db/seeds/04-users_answers_questions.js
+++ db/seeds/04-users_answers_questions.js
@@ -15,17 +15,48 @@ exports.seed = function(knex, Promise) {
 
       
 
-        {id: 7, user_id: 3, question_id: 1, answer:2},
-        {id: 8, user_id: 3, question_id: 2, answer:1},
+        {id: 7, user_id: 3, question_id: 1, answer:1},
+        {id: 8, user_id: 3, question_id: 2, answer:4},
         {id: 9, user_id: 3, question_id: 2, answer:2},
 
         {id: 10, user_id: 4, question_id: 1, answer:3},
-        {id: 11, user_id: 4, question_id: 2, answer:3},
-        {id: 12, user_id: 4, question_id: 3, answer:4},
+        {id: 11, user_id: 4, question_id: 2, answer:4},
+        {id: 12, user_id: 4, question_id: 3, answer:1},
 
-        {id: 13, user_id: 5, question_id: 1, answer:1},
+        {id: 13, user_id: 5, question_id: 1, answer:2},
         {id: 14, user_id: 5, question_id: 2, answer:4},
-        {id: 15, user_id: 5, question_id: 3, answer:1},
+        {id: 15, user_id: 5, question_id: 3, answer:3},
+
+        {id: 16, user_id: 6, question_id: 1, answer:3},
+        {id: 17, user_id: 6, question_id: 2, answer:2},
+        {id: 18, user_id: 6, question_id: 3, answer:4},
+
+        {id: 19, user_id: 7, question_id: 1, answer:1},
+        {id: 20, user_id: 7, question_id: 2, answer:4},
+        {id: 21, user_id: 7, question_id: 3, answer:1},
+
+        {id: 22, user_id: 8, question_id: 1, answer:3},
+        {id: 23, user_id: 8, question_id: 2, answer:1},
+        {id: 24, user_id: 8, question_id: 3, answer:2},
+
+
+        {id: 23, user_id: 9, question_id: 1, answer:2},
+        {id: 24, user_id: 9, question_id: 2, answer:4},
+        {id: 25, user_id: 9, question_id: 3, answer:1},
+
+
+        {id: 26, user_id: 10, question_id: 1, answer:2},
+        {id: 27, user_id: 10, question_id: 2, answer:2},
+        {id: 28, user_id: 10, question_id: 3, answer:4},
+
+        {id: 29, user_id: 11, question_id: 1, answer:1},
+        {id: 30, user_id: 11, question_id: 2, answer:3},
+        {id: 31, user_id: 11, question_id: 3, answer:4},
+
+
+
+
+
       ]).then(() => {
         return knex.raw(
           `SELECT setval('users_answers_questions_id_seq', (SELECT MAX(id) FROM users_answers_questions));`
