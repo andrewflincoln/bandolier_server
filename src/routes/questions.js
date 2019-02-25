aa7eb1a17cd8829463b32bb9dd34ceb5ef45813c
Andrew Lincoln <andrewjlincoln@gmail.com>
2019-02-24 22:31:25 -0800

    edit user done

--- src/routes/questions.js
+++ src/routes/questions.js
@@ -13,7 +13,7 @@ router.get('/:userId', questionsController.getUserQuestions) //all qs a user has
 router.get('/next/:userId', questionsController.getNextQuestion)
 router.get('/comp/:user1/:user2', questionsController.compAnswers)
 
-router.use('/', authController.authenticated, authController.isSelf )
+// router.use('/', authController.authenticated, authController.isSelf )
 router.post('/', questionsController.submitAnswer)
 
 
